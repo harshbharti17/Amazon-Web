f16afdec0bd0a7f3937c8ffb78c2058bc9441765
HarshBharti17 <harshbharti179@gmail.com>
2025-11-22 17:56:50 +0530

    rename all files structure

--- harsh.js
+++
@@ -1,4 +0,0 @@
-console.log("harsh");
-console.log("bharti")
-console.log("vijay");
-console.log("hitesh");
---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("harsh");
+console.log("bharti")
+console.log("vijay");
+console.log("hitesh");
